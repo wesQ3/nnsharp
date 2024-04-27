8adbf2a103f65fa05dc2c19f0b909704ebe089df
Wes Malone <wesQ3@users.noreply.github.com>
2024-04-27 11:56:28 -0500

    Just read a file, jeez

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using NumSharp;
+using System;
+using System.IO;
+
 Console.WriteLine("Hello, World!");
+
+var fs = File.OpenRead("data/train-labels-idx1-ubyte");
+for (int i = 0; i < 16; i++) {
+    var b = fs.ReadByte();
+    Console.WriteLine(b.ToString());
+}
+
+Console.WriteLine("done reading");
