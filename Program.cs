809b0113e09a60b1f93dd72e193ea08726bc3779
Wes Malone <wesQ3@users.noreply.github.com>
2024-04-27 22:43:48 -0500

    Read idx magic and dimensions

--- Program.cs
+++ Program.cs
@@ -1,13 +1,26 @@
 using NumSharp;
 using System;
+using System.Buffers.Binary;
 using System.IO;
+using System.Reflection;
 
-Console.WriteLine("Hello, World!");
+var filename = "data/train-images-idx3-ubyte";
+Console.WriteLine($"read from {filename}");
 
-var fs = File.OpenRead("data/train-labels-idx1-ubyte");
-for (int i = 0; i < 16; i++) {
-    var b = fs.ReadByte();
-    Console.WriteLine(b.ToString());
+var fs = File.OpenRead(filename);
+Console.WriteLine($"file size {fs.Length}");
+var br = new BinaryReader(fs);
+var magic = br.ReadBytes(4);
+Console.WriteLine($"magic: {Convert.ToHexString(magic)}");
+
+var dimensions = (int) magic[3];
+Console.WriteLine($"dimensions: {dimensions}");
+
+uint[] sizes = new uint[dimensions];
+for (int i = 0; i < dimensions; i++)
+{
+    var bigend = br.ReadBytes(4);
+    sizes[i] = BinaryPrimitives.ReadUInt32BigEndian(bigend);
+    Console.WriteLine($"size {i}: {sizes[i]}");
 }
-
-Console.WriteLine("done reading");
+Console.WriteLine("done");
