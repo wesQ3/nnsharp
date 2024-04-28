483db4ca783c490bfbefb6415104c19dbb24ce37
Wes Malone <wesQ3@users.noreply.github.com>
2024-04-28 00:10:21 -0500

    Create ndarray and reshape

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Buffers.Binary;
 using System.IO;
 using System.Reflection;
 
+// var filename = "data/train-labels-idx1-ubyte";
 var filename = "data/train-images-idx3-ubyte";
 Console.WriteLine($"read from {filename}");
 
@@ -16,11 +17,17 @@ Console.WriteLine($"magic: {Convert.ToHexString(magic)}");
 var dimensions = (int) magic[3];
 Console.WriteLine($"dimensions: {dimensions}");
 
-uint[] sizes = new uint[dimensions];
-for (int i = 0; i < dimensions; i++)
-{
+int[] sizes = new int[dimensions];
+for (int i = 0; i < dimensions; i++) {
     var bigend = br.ReadBytes(4);
-    sizes[i] = BinaryPrimitives.ReadUInt32BigEndian(bigend);
+    sizes[i] = (int) BinaryPrimitives.ReadUInt32BigEndian(bigend);
     Console.WriteLine($"size {i}: {sizes[i]}");
 }
+
+var total = sizes.Aggregate((acc, i) => acc * i);
+var body = br.ReadBytes(total);
+var array = np.array(body);
+var shape = new Shape(sizes);
+array.reshape(shape);
+Console.WriteLine("new shape: "+string.Join(",", shape));
 Console.WriteLine("done");
