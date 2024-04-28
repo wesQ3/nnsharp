70afd200165869e00551a532e6836949eb6b5c44
Wes Malone <wesQ3@users.noreply.github.com>
2024-04-28 16:31:48 -0500

    Add file loader function

--- Program.cs
+++ Program.cs
@@ -1,34 +1,8 @@
 using NumSharp;
-using System.Buffers.Binary;
 
-// var filename = "data/train-labels-idx1-ubyte";
-var filename = "data/train-images-idx3-ubyte";
-Console.WriteLine($"read from {filename}");
-
-var fs = File.OpenRead(filename);
-Console.WriteLine($"file size {fs.Length}");
-var br = new BinaryReader(fs);
-var magic = br.ReadBytes(4);
-Console.WriteLine($"magic: {Convert.ToHexString(magic)}");
-
-var dimensions = (int)magic[3];
-Console.WriteLine($"dimensions: {dimensions}");
-
-int[] sizes = new int[dimensions];
-for (int i = 0; i < dimensions; i++)
-{
-    var bigend = br.ReadBytes(4);
-    sizes[i] = (int)BinaryPrimitives.ReadUInt32BigEndian(bigend);
-    Console.WriteLine($"size {i}: {sizes[i]}");
-}
-
-var total = sizes.Aggregate((acc, i) => acc * i);
-var body = br.ReadBytes(total);
-var array = np.array(body);
-var shape = new Shape(sizes);
-array = array.reshape(shape);
-Console.WriteLine("new shape: " + string.Join(",", array.shape));
-var sample = array[1111];
+var trainLabels = DatasetLoader.LoadIdx("data/train-labels-idx1-ubyte");
+var trainImages = DatasetLoader.LoadIdx("data/train-images-idx3-ubyte");
+var sample = trainImages[1111];
 Console.WriteLine($"sample length: {sample.size}");
 if (np.isscalar(sample)) {
     Console.WriteLine(" 0: " + string.Join(",", sample.ToByteArray()));
